f827a4da3fa581415b1bd7f5f7b28825ebc8d4c7
joao <le.bezr@gmail.com>
2019-07-23 15:33:25 -0300

    js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# WordPress Plugins and Others" 
+#WordPress Plugins and Others
+
+Personal repo just for keeping track of things.
+
+I don't think anyone will every make use of this .
---
+++ contact-7-events-reponse/file.js
@@ -0,0 +1,55 @@
+!function(){
+  /**
+  -- WordPress Plugin
+  */
+
+  function createCookie(name, value, minutes) {
+    if (minutes) {
+      var date = new Date();
+      date.setTime(date.getTime()+(minutes*60*1000));
+      var expires = "; expires="+date.toGMTString();
+    } else {
+      var expires = "";
+    }
+    document.cookie = name+"="+value+expires+"; path=/";
+  }
+
+  // onLoad
+  document.addEventListener('load', () => {
+    const strictTo = document.querySelector('.conteudo');
+    if ( !strictTo ) return
+
+    const $form = strictTo.querySelector('.wpcf7');
+    const cookieLifeTime = Math.round(72 * 60); // Value must be in minutes
+
+
+    let cookie = document.cookie;
+    cookie = cookie.slice(cookie.indexOf('newsletter'));
+    cookie = cookie.includes('newsletter-status=true') ? true : false;
+
+    if (cookie){
+      deliver('https://unitech-rio.com.br/wp-content/uploads/2019/07/ITT_3rd_ESG_The_Role_of_Converged_and_Hyper_Converged_Infrastructure_Brief_BR.pdf');
+    } else {
+      console.warn('Usuário ainda não preencheu o formulário');
+    }
+
+    $form.addEventListener('wpcf7mailsent', e => {
+      createCookie( 'newsletter-status', 'true', cookieLifeTime);
+      deliver('https://unitech-rio.com.br/wp-content/uploads/2019/07/ITT_3rd_ESG_The_Role_of_Converged_and_Hyper_Converged_Infrastructure_Brief_BR.pdf');
+    })
+
+    function deliver(download){
+      const p = document.createElement('p');
+
+      let content =   `<a href="${download}" download>`;
+      content +=  ' Faça o download do pdf aqui';
+      content +=  '</a>';
+
+      p.className = 'nestedElement';
+      p.innerHTML = content;
+
+      jQuery('.conteudo').find('.contato p').eq(jQuery('.conteudo').find('.contato p').length-1).after(p);
+      console.warn('Deliverd PDF')
+    }
+  })
+}()
